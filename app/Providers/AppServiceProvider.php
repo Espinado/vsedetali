8bd569be9541185847f27f3bdf6686deee60be23
Corazon <rvr@arguss.lv>
2026-03-09 12:47:19 +0200

    second commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Older MySQL/MariaDB setups on shared hosting may reject utf8mb4 indexes at 255 chars.
+        Schema::defaultStringLength(191);
     }
 }
